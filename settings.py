9d1198fda3a9231597a8e1acaa4f2a89231607ff
shenyineng <ieiayaobb@qq.com>
2018-09-23 22:16:29 +0800

    adapt djangorestframework

--- settings.py
+++ settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    # 'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
