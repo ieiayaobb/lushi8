97c8e963adfd0f4f8a1e51ff7dcf75a7bd6cae06
shenyineng <ieiayaobb@qq.com>
2018-09-25 11:39:02 +0800

    add more fields for API

--- web/serializers.py
+++ web/serializers.py
@@ -3,3 +3,8 @@ from rest_framework import serializers
 class ChairmanSerializer(serializers.Serializer):
     id = serializers.CharField(read_only=True)
     name = serializers.CharField(max_length=256)
+    href = serializers.CharField(max_length=256)
+    title = serializers.CharField(max_length=256)
+    img = serializers.CharField(max_length=256)
+    num = serializers.CharField(max_length=256)
+    type = serializers.CharField(max_length=256)
